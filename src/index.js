d1024aa739ce835c39af1fcac12e2e1f774bd230
Yan Makouski <makovskiy.yan@gmail.com>
2018-10-07 21:32:14 +0300

    made 4 of 10

--- src/index.js
+++ src/index.js
@@ -1,18 +1,24 @@
 // DO WHATEVER YOU WANT HERE
 
-const createEnumerableProperty = () => {};
-const createNotEnumerableProperty = () => {};
+const createEnumerableProperty = function(x) {
+    let obj = {};
+    obj.x = 1;
+    return obj;
+};
+const createNotEnumerableProperty = function(x) {
+    return Symbol(x);
+};
 const createProtoMagicObject = () => {};
 const incrementor = () => {};
 const asyncIncrementor = () => {};
 const createIncrementer = () => {};
 
 // return same argument not earlier than in one second, and not later, than in two
-const returnBackInSecond = () => {};
+const returnBackInSecond = function(smth){
+}
 const getDeepPropertiesCount = () => {};
-const createSerializedObject = () => {};
-const toBuffer = () => {};
-const sortByProto = () => {};
+const createSerializedObject = () => null;
+const sortByProto = (objArr) => objArr.sort((a,b)=>{return a.__proto__ - b.__proto__});
 
 exports.createEnumerableProperty = createEnumerableProperty;
 exports.createNotEnumerableProperty = createNotEnumerableProperty;
